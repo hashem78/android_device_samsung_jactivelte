b8f67024ddb110cd9eedc946cd133406d91869b8
hashem78 <hashem.olayano@gmail.com>
2015-01-31 11:24:44 +0300

    Fix Wrong logcat device debug , and remove extra uneeded value

--- slim.mk
+++ slim.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=jactivelte TARGET_DEVICE=jactivelte \
-    TARGET_DEVICE=jfltespr \
+    PRODUCT_NAME=jactivelte
+    TARGET_DEVICE=jactivelte \
     BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:4.4.2/KTU84Q/I9295XXUCNE5:user/release-keys" \
     PRIVATE_BUILD_DESC="jactivelte-user 4.4.2 KTU84Q I9295XXUCNE5 release-keys"
 
